e3b39157acadc479a3cc4c5d76569aa50beb7de6
vippeople22 <46292914+vippeople22@users.noreply.github.com>
2021-02-17 13:21:04 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changecase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Begin with getting the text from the user
+            Console.WriteLine("Input text to be converted");
+            string ToBeConverted = (Console.ReadLine());
+            // Check if we want to convert to lower or uppercase
+            Console.WriteLine("Convert to Uppercase or Lowercase (u/l)");
+            string LowerOrUpper = (Console.ReadLine());
+
+            if (LowerOrUpper == "u")
+            {
+                Console.WriteLine("\r\n\n" + ToBeConverted.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine("\r\n\n" + ToBeConverted.ToLower());
+            }
+
+        }
+    }
+}
