4020f4476b56f347d663826c526b7f9c4d795192
vippeople22 <vippeople2@gmail.com>
2021-02-17 12:34:59 -0800

    Add project files.

---
+++ changecase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changecase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text to be converted");
+            string ToBeConverted = (Console.ReadLine());
+            
+        }
+    }
+}
